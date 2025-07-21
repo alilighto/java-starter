d8cf135719729c1a8636f86eb6111084115e75ae
mohammedouchkhi <mohammedouchkhi@gmail.com>
2025-07-21 13:10:06 +0100

    added variables and types

---
+++ Variables_types.java
@@ -0,0 +1,13 @@
+public class Variables_types {
+    public static void main(String[] args) {
+        char H = 'H';
+        byte three = 3;
+        short one = 1;
+        int zero = 0;
+        float two = 2.0f;
+        boolean bool = true;
+
+        String output = "" + H + three + one + one + zero + " w" + zero + "r" + one + "d " + two + " " + bool;
+        System.out.println(output);
+    }
+}
